5d6ea594a2aa3eae52beeeb4690b9557cf54c545
gorrotowi <t_o_wi@hotmail.com>
2014-11-13 05:32:09 -0600

    Calculando host y mascara de red

--- src/com/unam/subneteo/MainActivity.java
+++ src/com/unam/subneteo/MainActivity.java
@@ -3,11 +3,13 @@ package com.unam.subneteo;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	EditText edtxOct1, edtxOct2, edtxOct3, edtxOct4, edtxNumHost;
@@ -54,12 +56,56 @@ public class MainActivity extends Activity {
 					// esto es una IP valida de clase C
 					txtClaseValida.setText("Valida");
 					txtClaseValida.setTextColor(Color.GREEN);
+					calcularMascara();
 				} else {
 					txtClaseValida.setText("Invalida");
 					txtClaseValida.setTextColor(Color.RED);
+					Toast.makeText(MainActivity.this,
+							"Ingresa una IP de clase C valida",
+							Toast.LENGTH_SHORT).show();
 				}
 			}
 		});
+
+	}
+
+	public void calcularMascara() {
+		String tipoSubred = "";
+		int bits = Integer.parseInt(edtxNumHost.getText().toString()) - 24;
+		if (bits <= 0) {
+			Toast.makeText(MainActivity.this, "Mascara no valida",
+					Toast.LENGTH_SHORT).show();
+		} else {
+			int subredes = (int) Math.pow(2, bits);
+			int hostPorRed = 256 / subredes;
+			String primerosTresOctetos = oct1 + "." + oct2 + "." + oct3 + ".";
+			// String ultimoOcteto = oct4 + "";
+			int ultimoOctectoInt = oct4;
+			Log.w("Dime Subredes", "Subredes en total " + subredes);
+			Log.i("Dime los host por subredes", "Host total " + hostPorRed);
+
+			for (int i = 0; i < subredes; i++) {
+				int nombre = i * hostPorRed;
+				int primerHost = nombre + 1;
+				int ultimoHost = (hostPorRed * (i + 1)) - 2;
+				int broadCast = (hostPorRed * (i + 1)) - 1;
+				if (ultimoOctectoInt == nombre) {
+					tipoSubred = "Red";
+				} else if (ultimoOctectoInt == broadCast) {
+					tipoSubred = "BroadCast";
+				} else if (ultimoOctectoInt > primerHost
+						&& ultimoOctectoInt < ultimoHost) {
+					tipoSubred = "Host";
+				} else {
+					tipoSubred = "No se D:";
+				}
+				Log.d("Nombre de red", primerosTresOctetos + nombre);
+				Log.d("Nombre de primer Host", primerosTresOctetos + primerHost);
+				Log.d("Nombre de ultimo Host", primerosTresOctetos + ultimoHost);
+				Log.d("Nombre del BroadCast", primerosTresOctetos + broadCast);
+				Log.i("Tipo de red", tipoSubred);
+			}
+		}
 
 	}
 }
